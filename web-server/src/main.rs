c1862668cd9537c5cadbc184b9657b418e523058
Ruben <iruben229@gmail.com>
2020-11-25 17:16:05 -0500

    Simple request validation for / requests

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -95,7 +95,21 @@
     which means requesting 127.0.0.1:7878/something-else in the browser,
     will return the same HTML response
 
-    
+    VALIDATING THE REQUEST AND SELECTIVELY RESPONDING
+
+    Check that the browser is requesting / before returning the HTML file
+    and return an error if the browser requests anything else
+
+    get variable is hardcoded with data corresponding to the / request
+
+    Because raw bytes are being read from the buffer,
+    the get variable needs get transformed into a byte string
+    and in order to do so can add the b"" byte string syntax at the start of the content data
+
+    If the buffer starts with the bytes in get,
+    it means a well-formed request to / is received
+
+    The else block will return a response with the status code 404, along with some HTML
 ***/
 
 use std::io::prelude::*;
@@ -118,14 +132,28 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+    
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!(
+            "{}{}",
+            status_line,
+            contents
+        );
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
