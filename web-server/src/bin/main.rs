0c5777a3dcae7258500dd449933d0e08d9eb1c95
Ruben <iruben229@gmail.com>
2020-11-27 17:46:01 -0500

    Implement receiving and executing the jobs in the worker's thread

--- web-server/src/bin/main.rs
+++ web-server/src/bin/main.rs
@@ -48,6 +48,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use web_server::ThreadPool;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -62,6 +63,27 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}{}",
+        status_line,
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -85,11 +85,76 @@
 
     The Arc type will let multiple workers own the receiver,
     and Mutex will ensure that only one worker gets a job from the receiver at a time
+
+    IMPLEMENTING THE EXECUTE METHOD
+
+    Change Job from a struct to a type alias for a trait object that holds the type of closure that execute receives
+
+    After creating a new Job instance using the closure passed in the execute method,
+    the job is sent down the sending end of the channel
+
+    Calling unwrap on send for the case that sending fails,
+    which could happen if all threads stopped from executing,
+    meaning the receiving end has stopped receiving new messages
+    
+    The threads in this case cannot be stopped from executing;
+    as long as the pool exists it's fine
+
+    In the worker, the closure being passed to thread::spawn still only references the receiving end of the channel
+    Instead, the closure needs to loop forever,
+    asking the receiving end of the channel for a job
+    and running the job when it gets one
+
+    Inside the closure call lock on the receiver to acquire the mutex,
+    and then call unwrap to panic on any errors
+    Acquiring a lock might fail if the mutex is in a poisoned state,
+    which can happen if some other thread panicked while holding the lock rather than releasing the lock
+
+    If the lock on the mutex is acquired, call recv to receive a Job from the channel
+
+    A final unwrap moves past any errors here as well,
+    which might occur if the thread holding the sending side of the channel has shut down,
+    similar to how the send method returns Err if the receiving side shuts down
+
+    The call to recv blocks, so if there is no job yet, the current thread will wait until a job becomes available
+    
+    The Mutex<T> ensures that only one Worker thread at a time is trying to request a job
+
+    WHY A WHILE LET EXPRESSION WON'T RESULT IN THE DESIRED THREADING BEHAVIOR
+
+    while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {} got a job; executing.", id);
+
+                job();
+            }
+
+    the Mutex struct has no public unlock method
+    because the ownership of the lock is based on
+    the lifetime of the MutexGuard<T> within the LockResult<MutexGuard<T>> that the lock method returns
+
+    At compile time, the borrow checker can then enforce the rule
+    that a resource guarded by a Mutex cannot be accessed unless we hold the lock
+
+    But this implementation can also result in the lock being held longer than intended
+    if the lifetime of the MutexGuard<T> isn't carefully considered
+
+    Because the values in the while let expression remain in scope for the duration of the block,
+    the lock remains held for the duration of the call to job(),
+    meaning other workers cannot receive jobs
+
+    By using loop instead and acquiring the lock without assigning to a variable,
+    the temporary MutexGuard returned from the lock method is dropped as soon as the let job statement ends
+
+    This ensures that the lock is held during the call to recv,
+    but it is released before the call to job(),
+    allowing multiple requests to be serviced concurrently
 ***/
 
 use std::thread;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
@@ -99,10 +164,6 @@ pub struct ThreadPool {
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
-}
-
-struct Job {
-
 }
 
 impl ThreadPool {
@@ -132,12 +193,25 @@ impl ThreadPool {
             sender
         }
     }
+
+    pub fn execute<F>(&self, f: F)
+    where 
+        F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
         });
 
         Worker {
