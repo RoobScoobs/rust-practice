6fea2c42d2f57b06be5cf68198a5515f6b108e5c
Ruben <iruben229@gmail.com>
2020-12-03 18:51:43 -0500

    Finish adding the necessary fields and documentation to the App struct

--- hurl/src/app.rs
+++ hurl/src/app.rs
@@ -26,6 +26,30 @@
     The attributes on the fields are further used for customization
     For example, short, long says that the field should be usable
     using a short form like -q for quiet and a long form --quiet
+
+    An Option around a type signifies that argument is optional,
+    whereas a non-optional field that is not a bool would be treated as a required positional argument
+
+    The reason to use parse(from_occurrences) on the verbose field is to allow
+    the u8 field to be inferred from the number of times the argument is passed
+    This is a common pattern to use for verbosity by command line tools
+
+    A special field worth pointing out is cmd,
+    which has an attribute of subcommand
+
+    Basically this means that a Method type needs to be defined
+    which also derives StructOpt which will be used to create a subprogram here
+    As this is wrapped in an Option it's not required
+
+    The Vec<Parameter> type means that multiple values of this type of
+    input can be accepted
+
+    As this is a custom type (<Parameter>) parse_param also needs to be implemented in order
+    to work with the attribute that allows the definition of a custom parsing function
+
+    APP METHODS
+
+    Create a validate method to check whether a cmd or url exists
 ***/
 
 use log::{debug, trace};
@@ -38,50 +62,86 @@ use crate::errors::{Error, HurlResult};
 #[derive(StructOpt, Debug)]
 #[structopt(name = "hurl"))]
 pub struct App {
-/// Activate quiet mode
-/// 
-/// This overrides any verbose settings.
-#[structopt(short, long)]
-pub quiet: bool,
+    /// Activate quiet mode
+    /// 
+    /// This overrides any verbose settings.
+    #[structopt(short, long)]
+    pub quiet: bool,
 
-/// Verbose mode (-v, -vv, -vvv, etc.)
-#[structopt(short, long, parse(from_occurrences))]
-pub verbose: u8,
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short, long, parse(from_occurrences))]
+    pub verbose: u8,
 
-/// Form mode
-#[structopt(short, long)]
-pub form: bool,
+    /// Form mode
+    #[structopt(short, long)]
+    pub form: bool,
 
-/// Basic authentication
-/// 
-/// A string of the form `username:password`.
-/// If only `username` is given, then you will be prompted
-/// for a password. If you wish to use no password
-/// then use the form `username:`.
-#[structopt(short, long)]
-pub auth: Option<String>,
+    /// Basic authentication
+    /// 
+    /// A string of the form `username:password`.
+    /// If only `username` is given, then you will be prompted
+    /// for a password. If you wish to use no password
+    /// then use the form `username:`.
+    #[structopt(short, long)]
+    pub auth: Option<String>,
 
-/// Bearer token authenication
-/// 
-/// A token which will be sent as "Bearer <token>"
-/// in the authorization header.
-#[structopt(short, long)]
-pub token: Option<String>,
+    /// Bearer token authenication
+    /// 
+    /// A token which will be sent as "Bearer <token>"
+    /// in the authorization header.
+    #[structopt(short, long)]
+    pub token: Option<String>,
 
-/// Default transport
-/// 
-/// If a URL is given without a transport, i.e. example.com/foo
-/// http will be used as the transport by default.
-/// If this flag is set then https will be used instead.
-#[structopt(short, long)]
-pub secure: bool,
+    /// Default transport
+    /// 
+    /// If a URL is given without a transport, i.e. example.com/foo
+    /// http will be used as the transport by default.
+    /// If this flag is set then https will be used instead.
+    #[structopt(short, long)]
+    pub secure: bool,
 
-/// The HTTP Method to use, one of:
-/// HEAD, GET, POST, PUT, PATCH, DELETE.
-#[structopt(subcommand)]
-pub cmd: Option<Method>,
+    /// The HTTP Method to use, one of:
+    /// HEAD, GET, POST, PUT, PATCH, DELETE.
+    #[structopt(subcommand)]
+    pub cmd: Option<Method>,
 
-/// The URL to issue a request to
-/// if a method subcommand is not specified.
-pub url: Option<String>,
+    /// The URL to issue a request to
+    /// if a method subcommand is not specified.
+    pub url: Option<String>,
+
+    /// The parameters for the request if a method subcommand is not specified
+    /// 
+    /// There are seven types of parameters that can be added to a command-line.
+    /// Each type of parameter is distinguished by the unique separator between
+    /// the key and value
+    /// 
+    /// Header -- key:value
+    /// 
+    ///    e.g. X-API-TOKEN:abc123
+    /// 
+    /// File upload -- key@filename
+    /// 
+    ///    This simulates a file upload via multipart/form-data and requires --form
+    /// 
+    /// Query parameter -- key==value
+    /// 
+    ///    e.g. foo==bar becomes example.com?foo=bar
+    /// 
+    /// Data field -- key=value
+    /// 
+    ///    e.g. foo=bar becomes {"foo": "bar"} for JSON or form encoded
+    /// 
+    /// Data field from file -- key=@filename
+    /// 
+    ///    e.g. foo=@bar.txt becomes {"foo": "the contents of bar.txt"} or form encoded
+    /// 
+    /// Raw JSON data where the value should be parsed to JSON first -- key:=value
+    /// 
+    ///   e.g. foo:=[1,2,3] becomes {"foo": [1,2,3]}
+    /// 
+    /// Raw JSON data from file -- key:=@filename
+    /// 
+    ///   e.g. foo:=@bar.json becomes {"foo": {"bar": "this is from bar.json"}}
+    #[structopt(parse(try_from_str = parse_param))]
+    pub parameters: Vec<Parameter>,
 }
