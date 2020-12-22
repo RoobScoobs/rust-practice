0e06205312ce8205f874b7f04145afe2dc512997
Ruben <iruben229@gmail.com>
2020-12-21 23:20:26 -0500

    Writing the impl_builder_macro to connect to proc_macro2     | also including notes on the type signatures for the procedular macros

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -38,6 +38,90 @@
     syn::parse what type to turn the input into
 
     Docs on the syn crate: https://docs.rs/syn/1.0.55/syn/index.html
+
+    Using expect to panic if the input cannot be parsed
+    It is common to panic or use the compile_error macro in procedular macros
+
+    This is because the code is executing at compile time
+    so causing a panic is sometimes the only thing to do to stop the compilation process
+    when unable to proceed
+
+    THE proc_macro_derive ATTRIBUTE
+
+    This attribute takes the name of the derive as the first argument
+    which is Builder in this case
+
+    This makes so it's possible to write:
+        #[derive(Builder)]
+        struct Request {
+            ...
+        }
+
+    The second argument to proc_macro_derive is optional
+    and defines helper attributes
+
+    The syntax here is to write attributes(...) with a comma separated list of attributes
+    that are defined inside the parentheses
+
+    Here a single attribute called builder is defined so it's possible to write:
+        #[derive(Builder)]
+        struct Request {
+            #[builder]
+            pub x: i32,
+            ...
+        }
+
+    Attributes can also take arguments:
+        #[derive(Builder)]
+        struct Request {
+            #[builder(foo, bar=32)]
+            pub x: i32,
+            ...
+        }
+
+    TYPE SIGNATURE
+
+    The function builder_derive has the signature (TokenStream) -> TokenStream
+    which is the form that every custom derive must implement
+
+    The input is the item that the derive is defined on
+    and the return value is appended to the module or block
+    where that item is defined
+
+    Custom attribute macros have a signature of (TokenStream, TokenStream) -> TokenStream
+    where the first argument is the arguments to the attribute itself
+    and the second attribute is the item the attribute is on
+
+    The return value replaces the item with an arbitrary number of items
+
+    For example,
+        #[get("/")]
+        fn foobar() {}
+
+    means that there is a procedural macro that defines a function get
+    which will receive the token stream representing ("/") as its first argument
+    and the token stream representing fn foobar() {} as its second argument
+
+    The token stream output from that function will replace all of that code
+
+    Function-like procedular macros have the same signature as a derive macro
+    (TokenStream) -> TokenStream, where the input is the entirety of the macro invocation
+    but instead of getting appended to the module, the token stream that is returned
+    replaces the input at the same location in the source
+
+    THE impl_builder_macro FUNCTION
+
+    The purpose of this function is to move into the world of proc_macro2
+    by passing the syn input into a function which only operates in this other world
+
+    The into implementation of a type will help convert back into the proc_macro world
+    which is then used to retrun the expected TokenStream
+
+    The proc_macro2::TokenStream type impelements the Into trait to get a
+    proc_macro::TokenStream so the expectation is to get proc_macro2::TokenStream values
+    that simply need to call into on
+
+
 ***/
 
 extern crate proc_macro;
@@ -52,4 +136,11 @@ pub fn builder_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).expect("Could not parse type to derive Builder for");
 
     impl_builder_macro(ast)
+}
+
+fn impl_builder_macro(ty: syn::DeriveInput) -> TokenStream {
+    match parse_builder_information(ty) {
+        Ok(info) => info.into(),
+        Err(e) => to_compile_errors(e).into(),
+    }
 }
